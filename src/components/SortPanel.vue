5527ac811eebd38d9d87b99b9b3a0e7601cfa953
fyvl <yfyl77ov@gmail.com>
2024-09-03 14:52:15 +0700

    Initial

---
+++ src/components/SortPanel.vue
@@ -0,0 +1,48 @@
+<template>
+  <div class="sort-panel">
+    <v-tooltip text="Сортировать по рейтнгу (возрастание)" location="top">
+      <template v-slot:activator="{ props }">
+        <v-btn
+          v-bind="props"
+          icon="mdi-sort-ascending"
+          @click="$emit('sort', 'asc')"
+          variant="tonal"
+          color="white"
+        />
+      </template>
+    </v-tooltip>
+    <v-tooltip text="Сортировать по рейтнгу (убывание)" location="top">
+      <template v-slot:activator="{ props }">
+        <v-btn
+          v-bind="props"
+          icon="mdi-sort-descending"
+          @click="$emit('sort', 'desc')"
+          variant="tonal"
+          color="white"
+        />
+      </template>
+    </v-tooltip>
+    <v-tooltip text="Вернуть сортировку по умолчанию" location="top">
+      <template v-slot:activator="{ props }">
+        <v-btn
+          v-bind="props"
+          icon="mdi-sort"
+          @click="$emit('sort', 'none')"
+          variant="tonal"
+          color="white"
+        />
+      </template>
+    </v-tooltip>
+  </div>
+</template>
+
+<script setup>
+</script>
+
+<style scoped>
+  .sort-panel {
+    display: flex;
+    gap: 10px;
+    margin-bottom: 10px;
+  }
+</style>
